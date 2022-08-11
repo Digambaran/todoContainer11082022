877c5072d09ddb85d557d0e509aa5d1ee2adb0d5
Digambaran <arjunsreekumar.1@gmail.com>
2022-08-11 13:59:13 +0530

    fix: export name case

--- src/components/TodoInput11082022/TodoInput11082022.js
+++ src/components/TodoInput11082022/TodoInput11082022.js
@@ -1,26 +1,28 @@
-import React from 'react'
-import { useFederatedComponent } from 'yah-js-sdk'
+import React from "react";
+import { useFederatedComponent } from "yah-js-sdk";
 
 const TodoInput11082022 = (props) => {
   const system = {
     url: `${process.env.BLOCK_ENV_URL_todoInput11082022}/remoteEntry.js`,
-    scope: 'todoInput11082022',
-    module: './todoInput11082022',
-  }
+    scope: "todoInput11082022",
+    module: "./todoInput11082022",
+  };
 
   const { Component: FederatedComponent, errorLoading } = useFederatedComponent(
     system?.url,
     system?.scope,
     system?.module,
     React
-  )
+  );
 
   // console.log(FederatedComponent);
   return (
-    <React.Suspense fallback={''}>
-      {errorLoading ? `Error loading module "${module}"` : FederatedComponent && <FederatedComponent {...props} />}
+    <React.Suspense fallback={""}>
+      {errorLoading
+        ? `Error loading module "${module}"`
+        : FederatedComponent && <FederatedComponent {...props} />}
     </React.Suspense>
-  )
-}
+  );
+};
 
-export default todoInput11082022
+export default TodoInput11082022;
--- src/components/TodoItem11082022/TodoItem11082022.js
+++ src/components/TodoItem11082022/TodoItem11082022.js
@@ -1,26 +1,28 @@
-import React from 'react'
-import { useFederatedComponent } from 'yah-js-sdk'
+import React from "react";
+import { useFederatedComponent } from "yah-js-sdk";
 
 const TodoItem11082022 = (props) => {
   const system = {
     url: `${process.env.BLOCK_ENV_URL_todoItem11082022}/remoteEntry.js`,
-    scope: 'todoItem11082022',
-    module: './todoItem11082022',
-  }
+    scope: "todoItem11082022",
+    module: "./todoItem11082022",
+  };
 
   const { Component: FederatedComponent, errorLoading } = useFederatedComponent(
     system?.url,
     system?.scope,
     system?.module,
     React
-  )
+  );
 
   // console.log(FederatedComponent);
   return (
-    <React.Suspense fallback={''}>
-      {errorLoading ? `Error loading module "${module}"` : FederatedComponent && <FederatedComponent {...props} />}
+    <React.Suspense fallback={""}>
+      {errorLoading
+        ? `Error loading module "${module}"`
+        : FederatedComponent && <FederatedComponent {...props} />}
     </React.Suspense>
-  )
-}
+  );
+};
 
-export default todoItem11082022
+export default TodoItem11082022;
